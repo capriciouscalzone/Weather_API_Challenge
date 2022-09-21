b5092e5175e2697ac0f82f1128224ce5b01ecba7
Michael Crandall <mec192@gmail.com>
2022-09-21 16:59:05 -0500

    messing around with java trying to get it started

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
 var APIKey = "a7702cd16c3900bfd85bce8efc609fda";
 
-var city;
+var city = $("#cityName")
 
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
 fetch(queryURL)
+
