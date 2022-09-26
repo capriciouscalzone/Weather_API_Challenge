4b62fa7aff81f9103ff879f198ac9a18a05ef864
Michael Crandall <mec192@gmail.com>
2022-09-26 17:35:37 -0500

    added local storage and clear function for searched cities

--- assets/script.js
+++ assets/script.js
@@ -10,7 +10,27 @@ var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&ap
 $('#submitCity').click(function() {
     event.preventDefault();
     let cityName = $('#cityName').val();
+    getWeatherDetails(cityName);
+    getFutureWeather(cityName);
 })
+
+$('#pastCities').click(function() {
+    let cityName = event.target.value;
+    getWeatherDetails(cityName);
+    getFutureWeather(cityName);
+})
+
+if (localStorage.getItem('localWeatherSearches')) {
+    cityHistory = JSON.parse(localStorage.getItem('localWeatherSearches'));
+    writeSearchHistory(cityHistory);
+} else {
+    cityHistory = [];
+};
+$('#clear').click(function() {
+    localStorage.clear('localWeatherSEarches');
+});
+
+
 
 
 
